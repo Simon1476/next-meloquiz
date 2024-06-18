754eb9c50aebafd9b5d2e12b85b740c7b6622534
Simon1476 <77772647+Simon1476@users.noreply.github.com>
2024-06-18 14:43:27 +0900

    env: Rename environment variable

--- auth.config.ts
+++ auth.config.ts
@@ -4,8 +4,8 @@ import type { NextAuthConfig } from "next-auth";
 export default {
   providers: [
     Spotify({
-      clientId: process.env.SPOTIFY_CLIENT_ID,
-      clientSecret: process.env.SPOTIFY_CLIENT_SECRET,
+      clientId: process.env.AUTH_SPOTIFY_ID,
+      clientSecret: process.env.AUTH_SPOTIFY_SECRET,
       authorization:
         "https://accounts.spotify.com/authorize?scope=user-read-email,user-read-private,streaming,user-modify-playback-state,user-read-currently-playing,user-read-playback-state,user-library-read,user-library-modify,playlist-read-collaborative,playlist-read-private, playlist-modify-public",
     }),
